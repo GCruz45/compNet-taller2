91c5c69ecb67bbd5bfd9c1d555a18cc80b44a1f7
Gabriel Cruz <gabrielcl45@gmail.com>
2022-03-10 21:14:44 -0500

    Repo setup. Create initial project files

---
+++ src/main/java/com/example/demo/Tallet1A00056020Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tallet1A00056020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tallet1A00056020Application.class, args);
+	}
+
+}
